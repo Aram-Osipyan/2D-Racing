c641007eaeb61698d1f8da9356a5b194d9f65233
Aramcho <osipyan.aram2017@ya.ru>
2021-06-19 13:32:50 +0300

    add some sprites and make car moving by axis

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] float maxMotorTorque;
+    [SerializeField] float maxSteeringAngle;
+    [SerializeField] WheelJoint2D frontWheel;
+    [SerializeField] WheelJoint2D backWheel;
+    JointMotor2D jointMotor;
+    private void Start()
+    {
+        jointMotor = frontWheel.motor;
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        Debug.Log(motor + " " + steering);
+
+        jointMotor.motorSpeed = motor;
+        frontWheel.motor = jointMotor;
+        backWheel.motor = jointMotor;
+
+
+
+    }
+}
